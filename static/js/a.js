5906814e460aa8b0b72472d3c5c3cdd34b11d209
cwh <992916214@qq.com>
2017-07-18 12:20:55 +0800

    A demo
    A demo

--- static/js/a.js
+++ static/js/a.js
@@ -1,23 +1,129 @@
-var TestView = Backbone.View.extend({
-    template: null,  //  把模板直接放这里有时也可以，但有时又会出错，还不知道是什么原因
-    events: {
-        'click button#toggle' : 'toggle'
-    },
-    initialize: function() {
-        this.template = _.template($("#hello-template").html());  //  模板放这里一般没问题
-        this.render();
-    },
-    render: function() {
-        this.$el.html(this.template({message: "hello world!"}));  //  渲染模板
-        return this;
-    },
-    toggle: function() {
-        this.$("#hello").toggle();
-        return this;
-    }
-});
+window.onload = function () {
+    var oBtn = document.getElementById("txtclick");
+    var oImag = document.getElementById('imgclick');
+    var oDiv = document.getElementById("main-Panel");
+    var oBtnClear = document.getElementById("clearAll");
+    var oBtnSave = document.getElementById("save");
+    var oBtnPreview = document.getElementById('preview');
+    var iChange = document.querySelector("#imgChange");
+    var oChangeImg = document.getElementById("editorImg");
+    var oDel = document.getElementById('del');
 
-$(function () {
-    var v = new TestView({el: $('#body')});  //  以目标节点（一个 <div id="body" />）为el参数，创建一个view的实例，render函数将会被自动调用并将渲染结果填充到el中
-    //v.render(); // 如果没在 initialize 里调用 render 的话，就需要在这里调用一次
-});
+    var picArr = ['static/img/test1.jpg','static/img/test2.gif','static/img/test3.jpg','static/img/test4.jpg'];
+
+    if(oImag){
+        oImag.onclick = function () {
+            oDiv.innerHTML += '<li><div class="boxImg"><img src="static/img/test1.jpg" style="width: 300px;height: 400px;margin-left: 100px"></div></li><br/>';
+        }
+    }
+    if(oBtn){
+        document.getElementById("txtclick").onclick = function(){
+            oDiv.innerHTML+= '<li><div class="box">' + '请输入文本：'+'</div></li>';
+        };
+    }
+
+    var oLi = document.getElementsByClassName("items");
+    if(oLi){
+        for(var i=0; i<oLi.length; i++){
+            oLi[i].onclick = function () {
+                oDiv.innerHTML+= '<li><div class="box">'+'aaaa'+'</div></li>';
+            }
+        }
+    }
+    if(oBtnClear){
+        oBtnClear.onclick = function () {
+            oDiv.innerHTML='';
+        }
+    }
+
+    if(oBtnSave){
+        oBtnSave.onclick = function () {
+            if(oDiv.innerHTML){
+                localStorage.setItem("object", oDiv.innerHTML);
+                alert("保存成功");
+            }else {
+                alert("保存失败，没有获取到内容")
+            }
+        }
+    }
+    if(oBtnPreview){
+        oBtnPreview.onclick = function () {
+            var aLi = document.getElementById('main-Panel1').parentNode;
+            bootbox.dialog({
+                title: "预览作品",
+                message: '<ul>' + aLi.innerHTML + '</ul>'
+            });
+        }
+
+        oChangeImg.addEventListener('click',function () {
+            iChange.onclick = function () {
+                    oChangeImg.src = picArr[2];
+
+            }
+            oDel.onclick = function () {
+                oChangeImg.src = '';
+            }
+        })
+
+        oDiv.addEventListener('click',function () {
+            if(oDiv.children.tagName === 'LI'){
+
+            }
+        });
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
